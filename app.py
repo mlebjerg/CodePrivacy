32fd0ad79b59351dfbadfecb8248ad07bd1a7cc3
Mariana Meireles <mariana@psychonautgirl.space>
2019-07-11 23:15:49 +0200

    Added article, footer and header template

--- app.py
+++ app.py
@@ -4,3 +4,19 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('social_networks')
+def social_networks():
+    return 'social_networks'
+
+@app.route('personal_assistants')
+def personal_assistants():
+    return 'personal_assistants'
+
+@app.route('alexa')
+def alexa():
+    return 'alexa'
+
+@app.route('facebook')
+def facebook():
+    return 'facebook'
